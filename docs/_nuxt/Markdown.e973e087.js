c798e578fe916bcec1ec57102713bfcbb291d7c9
xsy <xushuyang@xcmg.com>
2024-06-12 15:40:13 +0800

    1

---
+++ docs/_nuxt/Markdown.e973e087.js
@@ -0,0 +1,1 @@
+import s from"./ContentSlot.101a67fa.js";import{e as o,f,p,A as u}from"./entry.4807ef02.js";import"./utils.041aeea4.js";const l=o({name:"Markdown",extends:s,setup(t){const{parent:e}=u(),{between:n,default:a}=f(),r=p(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:r,between:n,parent:e}}});export{l as default};
